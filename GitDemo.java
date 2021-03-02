e4bcf9a1aec8e7fd4528492783f190a966126e76
zmchugh <46854494+zmchugh@users.noreply.github.com>
2021-03-02 00:56:57 -0700

    added name in comments, and modified reverseNum() and findFactorial()

--- GitDemo.java
+++ GitDemo.java
@@ -1,10 +1,10 @@
 /*****
- * Edit this line to have your name.
+ Zachary McHugh
  ******/
 public class GitDemo {
     
     private double previousNumber;
-    
+    private double reverseNum
     public GitDemo() {
         previousNumber = 1;
     }
@@ -13,6 +13,7 @@ public class GitDemo {
      * This function should add 'num' to 'previousNumber' and return the sum.
     *****/
     public double addNumber(double num) {
+        previousNumber += num;
         System.out.print("Add num to previousNumber");
         return 0;
     }
@@ -45,7 +46,7 @@ public class GitDemo {
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
-        
+        reverseNum = newNumber;
         return newNumber;
     }
     
